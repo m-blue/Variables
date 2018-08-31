96e4d268f862675b785716edf23e66ca6e258f1f
MARK-PC\Mark <bigbluethe2@gmail.com>
2018-08-31 14:26:18 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        byte myByte = 1;
+        short myShort = 12;
+        int myInt = 123;
+        long myLong = 123456;
+        float myFloat = 12.32f;
+        double myDouble = 12.3456;
+        boolean myBoolean = true;
+        char myChar = 'a';
+
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("boolean: " + myBoolean);
+        System.out.println("char: " + myChar);
+
+
+    }
+}
